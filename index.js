9c8329f33e9573586bfefbec4e902f9004ef337d
TonMoshi <chingowamen@hotmail.com>
2020-05-23 16:27:46 +0200

    - Node express API init

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World, from express');
+});
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
